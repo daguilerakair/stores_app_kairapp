6b855b2575abd8d6cf0182f78624e2710665e787
daguilerakair <142628584+daguilerakair@users.noreply.github.com>
2024-02-20 18:28:20 -0300

    Update UserStoresTableSeeder.php

--- database/seeders/UserStoresTableSeeder.php
+++ database/seeders/UserStoresTableSeeder.php
@@ -12,25 +12,25 @@ class UserStoresTableSeeder extends Seeder
      */
     public function run(): void
     {
-        UserStore::create([
-            'user_id' => 1,
-            'store_rut' => 65555321,
-            'subStore_id' => null,
-            'admin' => true,
-            'role_id' => 2,
-            'status' => 1,
-            'delete' => 0,
-        ]);
+        //UserStore::create([
+        //  'user_id' => 1,
+        //  'store_rut' => 65555321,
+        //  'subStore_id' => null,
+        //  'admin' => true,
+        //  'role_id' => 2,
+        //  'status' => 1,
+        //  'delete' => 0,
+        //  ]);
 
-        UserStore::create([
-            'user_id' => 1,
-            'store_rut' => 77584357,
-            'subStore_id' => 3,
-            'admin' => false,
-            'role_id' => 4,
-            'status' => 1,
-            'delete' => 0,
-        ]);
+        //UserStore::create([
+        //  'user_id' => 1,
+        //  'store_rut' => 77584357,
+        //  'subStore_id' => 3,
+        //  'admin' => false,
+        //  'role_id' => 4,
+        //  'status' => 1,
+        //  'delete' => 0,
+        //  ]);
 
         UserStore::create([
             'user_id' => 3,
